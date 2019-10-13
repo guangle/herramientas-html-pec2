e90639b00650ebfd213ba84dd00b74003f1fd1cd
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2019-10-13 09:25:59 +0200

    sobreescribimos bs
    sobrescribimos bs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "bootstrap";
-import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
+//import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
 import "@fortawesome/fontawesome-free/css/all.css";
-import "/css/paleta.scss";
+
 import "/css/styles.scss";
 
 console.log("Inicio.");
