230fce41e1fc36917f7d63393971e4291785cc3d
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2019-10-13 09:08:36 +0200

    scss y algunas validaciones
    scss y algunas validaciones

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
 import "@fortawesome/fontawesome-free/css/all.css";
-import "/css/styles.css";
+import "/css/paleta.scss";
+import "/css/styles.scss";
 
 console.log("Inicio.");
