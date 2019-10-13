3f8c7d800804b1fcc9cd1d38cfc25eb83af48cb5
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2019-10-13 10:05:27 +0200

    eyupalc
    eyupalc

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "bootstrap";
-//import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
+// Import precompiled Bootstrap css
+//import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 import "/css/styles.scss";
