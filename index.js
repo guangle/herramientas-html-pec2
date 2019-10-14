20e71a30519edf89d81537115fdf7133b7f236a4
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2019-10-14 18:25:09 +0200

    restructuracion scss
    importamos todos desde sytles.scss

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+/*
 import "bootstrap";
 // Import precompiled Bootstrap css
 //import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 import "/css/styles.scss";
+*/
 
 console.log("Inicio.");
