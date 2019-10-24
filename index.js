dad4a6244f2561afd5704709f8d95799b5127285
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2019-10-24 06:47:59 +0200

    funcion respuesta
    respuesta

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 console.log("Herramientas front-end para dummies:");
 console.log("Inicio de la aplicacion.");
 
-
 /** Funcion que se ejecuta en los diferentes test que hay en la web,
  * se encarga, en funcion de si la respuesta es correcta o no, mostrar
  * diferentes animaciones en la tarjeta que contiene la pregunta y colorearla
@@ -11,23 +10,29 @@ console.log("Inicio de la aplicacion.");
  */
 
 var respuesta = function() {
-    var correcta = this.getAttribute("es-correcta");
-    console.log(this);
-    console.log(this.parentElement);
-    console.log('tarjeta');
-    console.log(this.parentElement.parentElement);
-    const tarjeta = this.parentElement.parentElement;
-    
-    tarjeta.classList.remove("shake-horizontal");
-    if( correcta == 1 ) {
-        alert('correcto');
-    } else {
-        //alert('error');
-        tarjeta.classList.add("shake-horizontal");
-    }
+  var correcta = this.getAttribute("es-correcta");
+  const tarjeta = this.parentElement.parentElement;
+
+  tarjeta.classList.remove("shake-horizontal");
+  tarjeta.classList.remove("jello-horizontal");
+  tarjeta.firstElementChild.classList.remove("bg-success");
+  tarjeta.firstElementChild.classList.remove("bg-danger");
+  var respuestas = tarjeta.getElementsByClassName("respuesta");
+  for (var i = 0; i < respuestas.length; i++) {
+    respuestas.item(i).classList.remove("font-weight-bold");
+  }
+
+  this.classList.add("font-weight-bold");
+  if (correcta == 1) {
+    tarjeta.classList.add("jello-horizontal");
+    tarjeta.firstElementChild.classList.add("bg-success");
+  } else {
+    tarjeta.classList.add("shake-horizontal");
+    tarjeta.firstElementChild.classList.add("bg-danger");
+  }
 };
 
 var respuestas = document.getElementsByClassName("respuesta");
 Array.from(respuestas).forEach(function(r) {
-    r.addEventListener('click', respuesta);
+  r.addEventListener("click", respuesta);
 });
