9a8600d48abbd3a13760ffee7045116ffecf00eb
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2020-01-05 12:06:25 +0100

    no purgamos las clases css que aplicamos a las tarjetas de respuesta

---
+++ purgecss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["**/*.html"],
+  whitelist: [
+    "shake-horizontal",
+    "jello-horizontal",
+    "bg-success",
+    "bg-danger",
+    "respuesta",
+    "font-weight-bold"
+  ]
+};
